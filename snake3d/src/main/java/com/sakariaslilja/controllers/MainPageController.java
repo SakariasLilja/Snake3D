9a917607cf0ccdd3adcf0f21244021a5a3813e50
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-20 17:26:35 +0200

    Added methods for navigation to other pages from main page

--- snake3d/src/main/java/com/sakariaslilja/controllers/MainPageController.java
+++ snake3d/src/main/java/com/sakariaslilja/controllers/MainPageController.java
@@ -6,9 +6,31 @@ import javafx.fxml.FXML;
 
 public class MainPageController {
 
+    /**
+     * Navigates via [App] to the view newgame.fxml.
+     * @throws IOException The file newgame.fxml wasn't found/couldn't be read.
+     */
     @FXML
     private void gotoNewGame() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("newgame");
+    }
+
+    /**
+     * Navigates via [App] to the view loadgame.fxml.
+     * @throws IOException The file loadgame.fxml wasn't found/couldn't be read.
+     */
+    @FXML
+    private void gotoLoadGame() throws IOException {
+        App.setRoot("loadgame");
+    }
+
+    /**
+     * Navigates via [App] to the view scoreboard.fxml.
+     * @throws IOException The file scoreboard.fxml wasn't found/couldn't be read.
+     */
+    @FXML
+    private void gotoScoreboard() throws IOException {
+        App.setRoot("scoreboard");
     }
     
 }
