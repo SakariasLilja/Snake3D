786ccb97d52df9b0648120d9762f8d2f7e410b41
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-20 15:48:33 +0200

    Added documentation and separated values to their own variables.

--- snake3d/src/main/java/com/sakariaslilja/App.java
+++ snake3d/src/main/java/com/sakariaslilja/App.java
@@ -9,28 +9,49 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * JavaFX App
+ * JavaFX App for the Snake3D game.
  */
 public class App extends Application {
 
     private static Scene scene;
 
+    // Variables of the scene.
+    private static final String primaryFXML = "primary";
+    private static final int width = 640;
+    private static final int height = 480;
+
+    // Sets the primary scene of the application.
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML(primaryFXML), width, height);
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Sets the root of the application's scene to be the inputed fxml-file.
+     * @param fxml File that is to be made the application's scene's root.
+     * @throws IOException File wasn't found/able to be read.
+     */
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    /**
+     * Method for loading a fxml-file.
+     * @param fxml The fxml-file to load.
+     * @return The loaded fxml-file.
+     * @throws IOException File wasn't found/able to be read.
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+    /**
+     * Main method that launches the application.
+     * @param args Default parameter for Java-applications.
+     */
     public static void main(String[] args) {
         launch();
     }
