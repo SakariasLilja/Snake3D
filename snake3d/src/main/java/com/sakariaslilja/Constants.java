52e9b6ae2a0e557cdff521695d6ec6f28b396c18
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-21 13:35:27 +0200

    Added documentation and world accuracy constant

--- snake3d/src/main/java/com/sakariaslilja/Constants.java
+++ snake3d/src/main/java/com/sakariaslilja/Constants.java
@@ -5,13 +5,40 @@ package com.sakariaslilja;
  */
 public class Constants {
 
+    /**
+     * The display's width in pixels.
+     */
     public static final int WIDTH = 640;
+
+    /**
+     * The display's height in pixels.
+     */
     public static final int HEIGHT = 480;
 
+    /**
+     * The game's unit size.
+     */
     public static final int UNIT = 1000;
+
+    /**
+     * The base movement speed of the snake.
+     * The amount the snake moves per frame.
+     */
     public static final int STEP_SIZE = 1;
 
+    /**
+     * The minimum size of any of the world's dimensions.
+     */
     public static final int MIN_WORLD_SIZE = 2;
+
+    /**
+     * The maximum size of any of the world's dimensions.
+     */
     public static final int MAX_WORLD_SIZE = 100;
+
+    /**
+     * The number of vertices (inclusive) between each main vertex.
+     */
+    public static final int WORLD_ACCURACY = 20;
     
 }
