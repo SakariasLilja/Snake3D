6ea05bce27e4bd670ba5de2c9a68a2df0f9f0314
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-21 12:54:08 +0200

    Added class for global constants

---
+++ snake3d/src/main/java/com/sakariaslilja/Constants.java
@@ -0,0 +1,14 @@
+package com.sakariaslilja;
+
+/**
+ * Class for defining global constants.
+ */
+public class Constants {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public static final int UNIT = 1000;
+    public static final int STEP_SIZE = 1;
+    
+}
