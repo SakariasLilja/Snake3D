2746ed7c39cc499455011c4e9cf55ab6e4189832
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-21 14:21:32 +0200

    Removed unused import

--- snake3d/src/test/java/com/sakariaslilja/models/WorldTests.java
+++ snake3d/src/test/java/com/sakariaslilja/models/WorldTests.java
@@ -1,7 +1,6 @@
 package com.sakariaslilja.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
