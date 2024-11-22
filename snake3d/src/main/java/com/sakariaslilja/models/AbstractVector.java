dc0c7fb05f967fe89a5aab3982a03cca5bb7d83d
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-22 14:07:43 +0200

    Added documentation

--- snake3d/src/main/java/com/sakariaslilja/models/AbstractVector.java
+++ snake3d/src/main/java/com/sakariaslilja/models/AbstractVector.java
@@ -1,7 +1,16 @@
 package com.sakariaslilja.models;
 
+/**
+ * Abstract class for Vectors.
+ * All vectors have only a scalar multiplication method
+ */
 public abstract class AbstractVector {
 
+    /**
+     * Scalar multiplication of a vector
+     * @param scalar The scalar with which to multiply this vector with
+     * @return A new AbstractVector instance with the new values
+     */
     public abstract AbstractVector mul(int scalar);
     
 }
