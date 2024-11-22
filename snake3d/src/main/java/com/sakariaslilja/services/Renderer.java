0a50ef4b0413f65bb61468d17a2de8bdb83dde45
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-22 19:12:39 +0200

    Added rendering methods and changed constructor

--- snake3d/src/main/java/com/sakariaslilja/services/Renderer.java
+++ snake3d/src/main/java/com/sakariaslilja/services/Renderer.java
@@ -1,6 +1,8 @@
 package com.sakariaslilja.services;
 
 import java.util.ArrayList;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import com.sakariaslilja.Constants;
 import com.sakariaslilja.models.DoubleVector3D;
@@ -17,26 +19,31 @@ public class Renderer {
     // Local variables
     private GraphicsContext g;
     private ArrayList<Tuple> edges;
-    private DoubleVector3D camera;
-    private DoubleVector3D normalVector;
+    private GameEngine engine;
+    private DoubleVector3D worldOffsetCorrection;
 
     private final Color backgroundColor = Color.web("#4a4e69");
-    //private final Color strokeColor = Color.web("#6495ED");
+    private final Color strokeColor = Color.web("#6495ED");
     //private final Color snakeColor = Color.web("#F5F5F5");
 
     /**
      * Renderer instance.
+     * Sets the world offset vector.
      * @param g The graphics context onto which to draw
      * @param edges The edges of the world
      * @param camera The camera of the world
      * @param normalVector The normal vector of the camera
      */
     @SuppressWarnings("exports")
-    public Renderer(GraphicsContext g, ArrayList<Tuple> edges, DoubleVector3D camera, DoubleVector3D normalVector) {
+    public Renderer(GraphicsContext g, ArrayList<Tuple> edges, GameEngine engine) {
         this.g = g;
         this.edges = edges;
-        this.camera = camera;
-        this.normalVector = normalVector;
+        this.engine = engine;
+
+        double offsetX = engine.getWorldWidth() * Constants.UNIT;
+        double offsetY = engine.getWorldHeight() * Constants.UNIT;
+        double offsetZ = engine.getWorldDepth() * Constants.UNIT;
+        this.worldOffsetCorrection = new DoubleVector3D(offsetX, offsetY, offsetZ);
     }
 
     /**
@@ -44,6 +51,7 @@ public class Renderer {
      */
     public void render() {
         this.clearCanvas(g);
+        this.drawEdges(g);
     }
 
     /**
@@ -54,5 +62,95 @@ public class Renderer {
         g.setFill(backgroundColor);
         g.fillRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
     }
+
+    /**
+     * Draws the edges onto the canvas
+     * @param g The canvas onto which to draw
+     */
+    private void drawEdges(GraphicsContext g) {
+        g.setStroke(strokeColor);
+        for (Tuple edge : edges) {
+            Tuple copy = edge.duplicate();
+            copy.forEach(applyMatricesFunc);
+            if (copy.forAll(hasPositiveZ)){
+                DoubleVector3D point1 = copy.value1;
+                DoubleVector3D point2 = copy.value2;
+
+                g.strokeLine(point1.getX(), point1.getY(), point2.getX(), point2.getY());
+            }            
+        }
+    }
+
+    /**
+     * Applies the translation matrix and world offset correction to the vertex.
+     * @param vertex Vertex to translate
+     * @return The translated vertex
+     */
+    private DoubleVector3D translationMatrix(DoubleVector3D vertex) {
+        return vertex.add(engine.getCamera().add(worldOffsetCorrection));
+    }
+
+    /**
+     * Gets the amount to rotate in each respective direction.
+     * @param camera The camera's position
+     * @param cameraNormal The camera's normal
+     * @return
+     */
+    private double[] getRotationAmount(DoubleVector3D camera, DoubleVector3D cameraNormal) {
+        // TODO: get calculations based on heading and normal
+        double[] rotations = {0, 0, 0};
+        return rotations;
+    }
+
+    /**
+     * Applies the rotation matrices to the vertex in the order:
+     * <p> rotZ, rotY, rotX
+     * @param vertex The vertex to rotate
+     * @return The rotated vertex
+     */
+    private DoubleVector3D applyRotations(DoubleVector3D vertex) {
+        double[] rotations = this.getRotationAmount(engine.getCamera(), engine.getCameraNormal());
+        return vertex.rotateZ(rotations[0]).rotateY(rotations[1]).rotateX(rotations[2]);
+    }
+
+    /**
+     * Applies the camera transform matrix
+     * @param vertex The vertex to transform
+     * @return The transformed vertex
+     */
+    private DoubleVector3D cameraTransform(DoubleVector3D vertex) {
+        double newX = vertex.getX() * Constants.FOCAL_LENGTH * Constants.WIDTH / (2 * Constants.SENSOR_SIZE_X);
+        double newY = vertex.getY() * Constants.FOCAL_LENGTH * Constants.HEIGHT / (2 * Constants.SENSOR_SIZE_Y);
+
+        return new DoubleVector3D(newX, newY, vertex.getY() + vertex.getZ());
+    }
+
+    /**
+     * Applies the perpective correction matrix
+     * @param vertex The vertex to correct
+     * @return The corrected vertex
+     */
+    private DoubleVector3D perpectiveCorrection(DoubleVector3D vertex) {
+        return new DoubleVector3D(vertex.getX() / vertex.getZ(), vertex.getY() / vertex.getZ(), vertex.getZ());
+    }
+
+    /**
+     * Applies every rendering matrix in order.
+     * @param vertex The vertex to render
+     * @return The rendered vertex
+     */
+    private DoubleVector3D applyMatrices(DoubleVector3D vertex) {
+        return perpectiveCorrection(cameraTransform(applyRotations(translationMatrix(vertex))));
+    }
+
+    /**
+     * Anonymous function that applies {@code applyMatrices} to a DoubleVector3D
+     */
+    private Function<DoubleVector3D, DoubleVector3D> applyMatricesFunc = (v) -> applyMatrices(v);
+
+    /**
+     * Predicate that checks if the z-coordinate of a DoubleVector3D is positive
+     */
+    private Predicate<DoubleVector3D> hasPositiveZ = (v) -> v.getZ() > 0;
     
 }
