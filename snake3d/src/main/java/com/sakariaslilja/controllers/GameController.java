ddc8d8f33725db00d211d40658ae549ed47e7751
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-22 17:09:32 +0200

    Added game clock, initialization method, and game engine and renderer dependency

--- snake3d/src/main/java/com/sakariaslilja/controllers/GameController.java
+++ snake3d/src/main/java/com/sakariaslilja/controllers/GameController.java
@@ -1,10 +1,46 @@
 package com.sakariaslilja.controllers;
 
 import com.sakariaslilja.App;
+import com.sakariaslilja.Constants;
+import com.sakariaslilja.services.GameEngine;
+import com.sakariaslilja.services.Renderer;
+
 import java.io.IOException;
+
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.VBox;
 
 public class GameController {
+
+    @FXML
+    Canvas gameCanvas;
+
+    @FXML
+    VBox bgoverlay = new VBox();
+
+    GameEngine engine;
+    Renderer renderer;
+
+    long startTime = System.nanoTime();
+    AnimationTimer gameClock = new AnimationTimer() {
+        @Override
+        public void handle(long arg0) {
+            long currentTime = System.nanoTime();
+            if (Constants.FPS <= (currentTime - startTime)) {
+                engine.update();
+                renderer.render();
+                startTime = currentTime;
+            }
+        };
+    };
+    
+    public void initialize() {
+        this.engine = App.getEngine();
+        this.renderer = new Renderer(gameCanvas.getGraphicsContext2D(), engine.getEdges(), engine.getCamera(), engine.getCameraNormal());
+        gameClock.start();
+    }
 
     /**
      * Navigates via [App] to mainpage.fxml.
