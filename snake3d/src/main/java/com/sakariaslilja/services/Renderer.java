1cd5fa0a3d115e519719657a33bfa0fd80558281
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-22 17:08:36 +0200

    Added functionality to renderer

--- snake3d/src/main/java/com/sakariaslilja/services/Renderer.java
+++ snake3d/src/main/java/com/sakariaslilja/services/Renderer.java
@@ -1,8 +1,10 @@
 package com.sakariaslilja.services;
 
+import java.util.ArrayList;
+
 import com.sakariaslilja.Constants;
 import com.sakariaslilja.models.DoubleVector3D;
-import com.sakariaslilja.models.World;
+import com.sakariaslilja.models.Tuple;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -13,22 +15,40 @@ import javafx.scene.paint.Color;
 public class Renderer {
 
     // Local variables
-    private final Color backgroundColor = Color.web("#6495ED");
+    private GraphicsContext g;
+    private ArrayList<Tuple> edges;
+    private DoubleVector3D camera;
+    private DoubleVector3D normalVector;
+
+    private final Color backgroundColor = Color.RED; //Color.web("#4a4e69");
+    //private final Color strokeColor = Color.web("#6495ED");
+    //private final Color snakeColor = Color.web("#F5F5F5");
+
+    /**
+     * Renderer instance.
+     * @param g The graphics context onto which to draw
+     * @param edges The edges of the world
+     * @param camera The camera of the world
+     * @param normalVector The normal vector of the camera
+     */
+    @SuppressWarnings("exports")
+    public Renderer(GraphicsContext g, ArrayList<Tuple> edges, DoubleVector3D camera, DoubleVector3D normalVector) {
+        this.g = g;
+        this.edges = edges;
+        this.camera = camera;
+        this.normalVector = normalVector;
+    }
 
     /**
      * Render function that calls all other functions of Renderer in order.
-     * @param g The graphics context onto which to render
-     * @param world The world used for calculations
-     * @param camera The camera location from which to render
-     * @param normalVector The camera's normal vector
      */
-    public void render(GraphicsContext g, World world, DoubleVector3D camera, DoubleVector3D normalVector) {
+    public void render() {
         this.clearCanvas(g);
     }
 
     /**
      * Clears the canvas by filling it completely with background color
-     * @param g
+     * @param g The graphics context onto which to render
      */
     private void clearCanvas(GraphicsContext g) {
         g.setFill(backgroundColor);
