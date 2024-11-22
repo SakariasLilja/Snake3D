d3975362d41b67e34f661304347b4cbbe197a78b
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-22 23:17:45 +0200

    Stopped visual artifacts from occuring when z-coordinate is too close to 0

--- snake3d/src/main/java/com/sakariaslilja/services/Renderer.java
+++ snake3d/src/main/java/com/sakariaslilja/services/Renderer.java
@@ -160,6 +160,6 @@ public class Renderer {
     /**
      * Predicate that checks if the z-coordinate of a DoubleVector3D is positive
      */
-    private Predicate<DoubleVector3D> hasPositiveZ = (v) -> v.getZ() > 0;
+    private Predicate<DoubleVector3D> hasPositiveZ = (v) -> v.getZ() > 0.000001;
     
 }
