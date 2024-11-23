99956c4e27edc6a380a944349508bb61993df0cf
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-23 22:14:53 +0200

    Added model for setting creation

---
+++ snake3d/src/main/java/com/sakariaslilja/models/SettingsModel.java
@@ -0,0 +1,17 @@
+package com.sakariaslilja.models;
+
+/**
+ * Model for game settings.
+ * Used by Gson.
+ */
+public class SettingsModel {
+
+    // Default values
+    public String validate = "validate";
+    public String backgroundColor = "#4A4E69";
+    public String textColor = "#6495ED";
+    public String edgeColor = "#6495ED";
+    public String snakeColor = "#F5F5F5";
+    public String appleColor = "#00CCCC";
+    
+}
