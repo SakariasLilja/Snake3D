f90af74c253e9a944ac1c2f9d0c98610511840d2
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-23 20:11:43 +0200

    Changed dependencies to work

--- snake3d/src/main/java/module-info.java
+++ snake3d/src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module com.sakariaslilja {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-    opens com.sakariaslilja to javafx.fxml;
+    opens com.sakariaslilja to javafx.fxml, gson;
     opens com.sakariaslilja.controllers to javafx.fxml;
-    opens com.sakariaslilja.services to javafx.fxml;
+    opens com.sakariaslilja.services to javafx.fxml, gson;
     opens com.sakariaslilja.models to javafx.fxml;
+    
     exports com.sakariaslilja;
     exports com.sakariaslilja.controllers;
     exports com.sakariaslilja.services;
