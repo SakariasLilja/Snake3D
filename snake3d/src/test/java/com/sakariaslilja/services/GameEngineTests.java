17915f2d3cc62a3e6db8bb21211f3826cc2b77dd
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-24 22:52:13 +0200

    Added tests for game engine

--- snake3d/src/test/java/com/sakariaslilja/services/GameEngineTests.java
+++ snake3d/src/test/java/com/sakariaslilja/services/GameEngineTests.java
@@ -3,10 +3,14 @@ package com.sakariaslilja.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.sakariaslilja.entities.Apple;
 import com.sakariaslilja.models.GameModel;
+import com.sakariaslilja.models.Vector3D;
 
 public class GameEngineTests {
 
@@ -31,6 +35,26 @@ public class GameEngineTests {
 
         assertEquals(createdModel.seed, model.seed, "The toGameModel should work as expected");
         assertNotEquals(model.score, createdModel.score, "The score should be different after incrementation");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @DisplayName("GameEngine getAvailableGridPositions")
+    void getAvailableGridPositions() {
+        GameEngine engine = new GameEngine(new GameModel(), null);
+        Vector3D position = new Vector3D(0, 0, 0);
+
+        ArrayList<Apple> apples = new ArrayList<>();
+        Apple apple = new Apple(position);
+        apples.add(apple);
+
+        ArrayList<Vector3D> allPositions = engine.getAvailableGridPositions();
+        ArrayList<Vector3D> appleOccupied = engine.getAvailableGridPositions(apples);
+
+        assertNotEquals(allPositions.size(), appleOccupied.size(), "Passing elements to remove should remove said elements");
+        int index = allPositions.indexOf(position);
+        assertNotEquals(-1, index, "The position should exist in the original array");
+        assertNotEquals(allPositions.get(index), appleOccupied.get(index), "The position at the deleted index should not be the same");
     }
     
 }
