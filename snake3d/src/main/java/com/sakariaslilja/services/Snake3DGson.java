37f29ee9709f3c74c5a67c3f05c49cb916dc54dd
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-24 21:40:37 +0200

    Added gson object interface

---
+++ snake3d/src/main/java/com/sakariaslilja/services/Snake3DGson.java
@@ -0,0 +1,17 @@
+package com.sakariaslilja.services;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Interface for the Gson object used by the game
+ */
+public interface Snake3DGson {
+
+    /**
+     * The Gson service used by the application I/O services
+     */
+    @SuppressWarnings("exports")
+    Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create();
+    
+}
