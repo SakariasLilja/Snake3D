ad07b97c82bfbdf6ec7aef9d7deba6688ee3adb1
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-24 23:51:19 +0200

    Added renderer tests

---
+++ snake3d/src/test/java/com/sakariaslilja/services/RendererTests.java
@@ -0,0 +1,54 @@
+package com.sakariaslilja.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.sakariaslilja.models.DoubleVector3D;
+import com.sakariaslilja.models.GameModel;
+
+public class RendererTests {
+
+    @Test
+    @DisplayName("Renderer translate")
+    void renderTranslate() {
+        DoubleVector3D camera = new DoubleVector3D(1, 1, 1);
+        GameEngine engine = new GameEngine(new GameModel(), camera);
+        Renderer renderer = new Renderer(null, engine);
+
+        DoubleVector3D vertex = new DoubleVector3D(0, 0, 0);
+        DoubleVector3D expected = new DoubleVector3D(-1, -1, -1);
+
+        assertEquals(expected, renderer.translate(vertex), "The translate method should work as expected");
+    }
+
+    @Test
+    @DisplayName("Renderer rotations")
+    void renderRotations() {
+        GameModel model = new GameModel();
+        model.rotX = 90;
+        model.rotY = 90;
+        model.rotZ = 90;
+        GameEngine engine = new GameEngine(model, null);
+        Renderer renderer = new Renderer(null, engine);
+
+        DoubleVector3D vertex = new DoubleVector3D(1, 2, 3);
+        DoubleVector3D expected = new DoubleVector3D(3, -2, 1);
+
+        assertEquals(expected, renderer.rotate(vertex).round(), "The rotate method should apply the matrices correctly");
+    }
+
+    @Test
+    @DisplayName("Renderer perspectiveCorrection")
+    void rendererPerspectiveCorrection() {
+        GameEngine engine = new GameEngine(new GameModel(), null);
+        Renderer renderer = new Renderer(null, engine);
+
+        DoubleVector3D vertex = new DoubleVector3D(8, -4, 2);
+        DoubleVector3D expected = new DoubleVector3D(4, -2, 2);
+
+        assertEquals(expected, renderer.perspectiveCorrection(vertex), "The perspective correction should work as expected");
+    }
+    
+}
