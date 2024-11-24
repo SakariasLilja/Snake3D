a3f7ede005830d4d52a98c02676dd42842e7ddd7
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-24 21:04:27 +0200

    Added opens and exports for entities

--- snake3d/src/main/java/module-info.java
+++ snake3d/src/main/java/module-info.java
@@ -7,9 +7,11 @@ module com.sakariaslilja {
     opens com.sakariaslilja.controllers to javafx.fxml;
     opens com.sakariaslilja.services to javafx.fxml, gson;
     opens com.sakariaslilja.models to javafx.fxml;
+    opens com.sakariaslilja.entities to javafx.fxml;
     
     exports com.sakariaslilja;
     exports com.sakariaslilja.controllers;
     exports com.sakariaslilja.services;
     exports com.sakariaslilja.models;
+    exports com.sakariaslilja.entities;
 }
