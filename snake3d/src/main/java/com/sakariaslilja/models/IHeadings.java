950c22ef2c7634fc5cf3aa560c475d0e5d77f6ed
SakariasLilja <sakarias.lilja@gmail.fi>
2024-11-27 23:00:29 +0200

    Added IHeadings for representing headings with Vector3Ds

---
+++ snake3d/src/main/java/com/sakariaslilja/models/IHeadings.java
@@ -0,0 +1,12 @@
+package com.sakariaslilja.models;
+
+public interface IHeadings {
+
+    Vector3D FORWARD    = new Vector3D(0, 0, 1);
+    Vector3D BACKWARD   = new Vector3D(0, 0, -1);
+    Vector3D LEFT       = new Vector3D(-1, 0, 0);
+    Vector3D RIGHT      = new Vector3D(1, 0, 0);
+    Vector3D UP         = new Vector3D(0, -1, 0);
+    Vector3D DOWN       = new Vector3D(0, 1, 0);
+    
+}
