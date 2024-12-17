0ddbde731f8de0d36bc1c96e6a3aeaff101c2e11
SakariasLilja <sakarias.lilja@gmail.fi>
2024-12-17 16:16:11 +0200

    Added serialization tags

--- snake3d/src/main/java/com/sakariaslilja/entities/Turn.java
+++ snake3d/src/main/java/com/sakariaslilja/entities/Turn.java
@@ -1,5 +1,6 @@
 package com.sakariaslilja.entities;
 
+import com.google.gson.annotations.SerializedName;
 import com.sakariaslilja.datastructures.IHeading;
 import com.sakariaslilja.datastructures.Vector3D;
 
@@ -7,10 +8,15 @@ import com.sakariaslilja.datastructures.Vector3D;
  * Defines the direction for turning a movable object.
  */
 public enum Turn implements IHeading {
+    @SerializedName("1")
     L(LEFT),
+    @SerializedName("2")
     R(RIGHT),
+    @SerializedName("3")
     U(UP),
+    @SerializedName("4")
     D(DOWN),
+    @SerializedName("0")
     N(FORWARD);
 
     /**
