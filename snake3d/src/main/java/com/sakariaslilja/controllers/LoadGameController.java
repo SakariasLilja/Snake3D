b249fab483fb88c4758cb0e75049052b73cd7cc6
SakariasLilja <sakarias.lilja@gmail.fi>
2024-12-25 18:05:48 +0200

    Modified load game view and controller

--- snake3d/src/main/java/com/sakariaslilja/controllers/LoadGameController.java
+++ snake3d/src/main/java/com/sakariaslilja/controllers/LoadGameController.java
@@ -1,14 +1,36 @@
 package com.sakariaslilja.controllers;
 
 import com.sakariaslilja.App;
+import com.sakariaslilja.models.GameModel;
+import com.sakariaslilja.services.GamesService;
+
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 /**
  * Controller for loadgame.fxml.
  * View for loading a previously saved game.
  */
 public class LoadGameController {
+
+    ObservableList<GameModel> games = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<GameModel> gamesList;
+
+    /**
+     * Method called when the scene is loaded.
+     */
+    public void initialize() {
+        gamesList.setItems(games);
+        ArrayList<GameModel> savedGames = GamesService.getGames();
+        savedGames.forEach(g -> games.add(g));
+    }
 
     /**
      * Navigates via [App] to mainpage.fxml
