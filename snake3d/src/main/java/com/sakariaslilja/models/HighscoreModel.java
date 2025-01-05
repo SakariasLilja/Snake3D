bb2a88e7c498b93099b0d9d82b8bde606d9c3f75
SakariasLilja <sakarias.lilja@gmail.fi>
2025-01-05 13:44:38 +0200

    Added highscore model

---
+++ snake3d/src/main/java/com/sakariaslilja/models/HighscoreModel.java
@@ -0,0 +1,20 @@
+package com.sakariaslilja.models;
+
+import java.util.Date;
+
+import com.sakariaslilja.IConstants;
+
+/**
+ * Model for a highscore set in the game.
+ * Each ended game will have a final score, a date when the game was ended,
+ * and the dimensions of the world the score was set in.
+ */
+public class HighscoreModel implements IConstants {
+
+    public int score = 0;
+    public Date date = new Date();
+    public int worldWidth = MIN_WORLD_SIZE;
+    public int worldHeight = MIN_WORLD_SIZE;
+    public int worldDepth = MIN_WORLD_SIZE;
+    
+}
