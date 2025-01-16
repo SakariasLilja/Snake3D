864c33cee65d9877c4a9cab45adef639f9c2ef74
SakariasLilja <sakarias.lilja@gmail.com>
2025-01-16 15:41:45 +0200

    Added documentation and changed variable visibility

--- snake3d/src/main/java/com/sakariaslilja/services/GameClock.java
+++ snake3d/src/main/java/com/sakariaslilja/services/GameClock.java
@@ -1,18 +1,26 @@
 package com.sakariaslilja.services;
 
-
 import com.sakariaslilja.IConstants;
 
 import javafx.animation.AnimationTimer;
 import javafx.scene.control.Label;
 
+/**
+ * Clock that runs the game's update cycle
+ */
 public class GameClock extends AnimationTimer implements IConstants {
 
-    GameEngine engine;
-    Renderer renderer;
-    Label score;
-    long startTime;
+    private GameEngine engine;
+    private Renderer renderer;
+    private Label score;
+    private long startTime;
 
+    /**
+     * Instantiates the clock to to a game instance and a renderer
+     * @param engine The engine to be updated
+     * @param renderer The renderer to be updated
+     * @param score The score label to be updated
+     */
     public GameClock(GameEngine engine, Renderer renderer, Label score) {
         this.engine = engine;
         this.renderer = renderer;
@@ -29,6 +37,5 @@ public class GameClock extends AnimationTimer implements IConstants {
             score.setText("" + engine.getScore());
             startTime = currentTime;
         }
-    }
-    
+    } 
 }
