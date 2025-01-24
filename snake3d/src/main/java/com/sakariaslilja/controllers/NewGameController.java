fa230262b22f72bbcf8d9e5d6cd8d084fe9e953e
SakariasLilja <sakarias.lilja@gmail.com>
2025-01-24 20:14:35 +0200

    Bound elements between view and controller

--- snake3d/src/main/java/com/sakariaslilja/controllers/NewGameController.java
+++ snake3d/src/main/java/com/sakariaslilja/controllers/NewGameController.java
@@ -3,12 +3,37 @@ package com.sakariaslilja.controllers;
 import com.sakariaslilja.App;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 /**
  * Controller for newgame.fxml.
  * View for starting a new game and giving it parameters.
  */
 public class NewGameController {
+
+    @FXML
+    Button cancelbtn;
+
+    @FXML
+    Button createbtn;
+
+    @FXML
+    Button incrwbtn;
+
+    @FXML
+    Button decrwbtn;
+
+    @FXML
+    Button incrhbtn;
+
+    @FXML
+    Button decrhbtn;
+
+    @FXML
+    Button incrdbtn;
+
+    @FXML
+    Button decrdbtn;
 
     /**
      * Navigates via [App] to mainpage.fxml
@@ -24,7 +49,7 @@ public class NewGameController {
      * @throws IOException The file game.fxml wasn't found/couldn't be read.
      */
     @FXML
-    private void gotoGame() throws IOException {
+    private void createGame() throws IOException {
         App.setRoot("game");
     }
     
