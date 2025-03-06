ab7423d5c5e5e8b5fba1ee2577406c5cdf122a5f
SakariasLilja <sakarias.lilja@gmail.com>
2025-03-06 23:30:51 +0200

    Added dimension modification functions

--- snake3d/src/main/java/com/sakariaslilja/controllers/NewGameController.java
+++ snake3d/src/main/java/com/sakariaslilja/controllers/NewGameController.java
@@ -1,15 +1,19 @@
 package com.sakariaslilja.controllers;
 
 import com.sakariaslilja.App;
+import com.sakariaslilja.IConstants;
+
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * Controller for newgame.fxml.
  * View for starting a new game and giving it parameters.
  */
-public class NewGameController {
+public class NewGameController implements IConstants {
 
     @FXML
     Button cancelbtn;
@@ -34,6 +38,62 @@ public class NewGameController {
 
     @FXML
     Button decrdbtn;
+
+    @FXML
+    Label widthLabel;
+
+    @FXML
+    Label heightLabel;
+
+    @FXML
+    Label depthLabel;
+
+    private Integer width = MIN_WORLD_SIZE;
+    private Integer height = MIN_WORLD_SIZE;
+    private Integer depth = MIN_WORLD_SIZE;
+
+    /** Called upon launch. Initializes integer properties */
+    public void initialize() {
+        widthLabel.setText(width.toString());
+        heightLabel.setText(height.toString());
+        depthLabel.setText(depth.toString());
+    }
+
+    @FXML
+    private void increaseWidth() {
+        width = Math.min(MAX_WORLD_SIZE, width + 1);
+        widthLabel.setText(width.toString());
+    }
+
+    @FXML
+    private void decreaseWidth() {
+        width = Math.max(MIN_WORLD_SIZE, width - 1);
+        widthLabel.setText(width.toString());
+    }
+
+    @FXML
+    private void increaseHeight() {
+        height = Math.min(MAX_WORLD_SIZE, height + 1);
+        heightLabel.setText(height.toString());
+    }
+
+    @FXML
+    private void decreaseHeight() {
+        height = Math.max(MIN_WORLD_SIZE, height - 1);
+        heightLabel.setText(height.toString());
+    }
+
+    @FXML
+    private void increaseDepth() {
+        depth = Math.min(MAX_WORLD_SIZE, depth + 1);
+        depthLabel.setText(depth.toString());
+    }
+
+    @FXML
+    private void decreaseDepth() {
+        depth = Math.max(MIN_WORLD_SIZE, depth - 1);
+        depthLabel.setText(depth.toString());
+    }
 
     /**
      * Navigates via [App] to mainpage.fxml
